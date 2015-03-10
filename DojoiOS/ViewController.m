4992ff395e601a2b7b83df57ad711b444cc1122b
Gustavo Gardenal Barbosa <gustavob@MACCIT012134.local>
2015-03-10 11:59:41 -0300

    Initial Commit

---
+++ DojoiOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DojoiOS
+//
+//  Created by Gustavo Gardenal Barbosa on 3/10/15.
+//  Copyright (c) 2015 ciandt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DojoiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DojoiOS
+//
+//  Created by Gustavo Gardenal Barbosa on 3/10/15.
+//  Copyright (c) 2015 ciandt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
